5463c528882e6f6de3302bf105c704dba89f1ea6
rabbit <rabbit@100.64.9.200>
2020-08-31 13:49:50 -0700

    asgn3 upload

---
+++ Assignments/Assignment3.java
@@ -0,0 +1,43 @@
+/*
+Assignment #: 3
+        Name: Yongsung Park
+   StudentID: 1218042098
+     Lecture: MWF 9:40-10:30 AM
+
+1. Pursue high quality in every aspect of the work. - 2.1
+- Though it may not be possible to achieve the ultimately idealistic value in 
+every work, as a professional one must always commit to the best possible design
+throughout the process of work. For example, while designing a code for an 
+inventory application in java, one should consider the reusability of the code 
+and use reasonable number of different classes, rather than cramming all 
+functionality in one single class.
+
+2. Be open to criticism and opinions.   - 2.4
+- The quality of programming work can be greatly influenced and improved by the
+review of peer professionals. Even though collaboration on assignments in CSE205
+is strictly prohibited, students can improve their code by reviewing each 
+other's codes and exchanging varying perspectives and approaches to a shared
+objective.
+
+3. Understand your capabilities and contribute to a field of such. - 2.6
+- A programmer should be aware of their scope of expertise and make ethical 
+judgement when accepting a computing assignment. If a Java programmer is 
+assigned a work that entails coding in a language one is unfamiliar with, the
+programmer should clearly notify the employer of one's incompetence.
+
+4. Computing work must prioritize the common benefit. - 3.1
+- Central or peripheral, a group that is involved with the work should be the 
+main concern in the process and result of a programming work. In other words, 
+a programmer's personal interest or private relation shall not disrupt the 
+common good that may be affected by the programming work.
+
+5. Consider compatibility of a software system. - 3.6
+- A simplest change made to an existing source code can drastically change its
+utility and impact. A programmer must take into consideration the legacy 
+compatibility as well as the ramification of the addition/modification when 
+writing codes. When Apple retired Aperture, an image editing and organization 
+application, they provided adequate support for users to migrate to different 
+systems such as their new Photos app or Adobe Lightroom.
+
+
+*/
